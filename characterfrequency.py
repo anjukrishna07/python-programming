283db1212dd3f05259eb4742d66840b0ae07f50a
anjukrishna07 <117815090+anjukrishna07@users.noreply.github.com>
2022-12-14 10:42:40 +0530

    Update characterfrequency.py

--- characterfrequency.py
+++ characterfrequency.py
@@ -5,3 +5,7 @@ def charFreq(str):
     return wordcount
 word = input("Enter a string to check for character frequency:")
 print(charFreq(word))
+//
+output
+Enter a string to check for character frequency:apple
+{'p': 2, 'e': 1, 'l': 1, 'a': 1}
